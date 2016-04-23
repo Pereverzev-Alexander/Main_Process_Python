f7d08b156668d36b674b68757908f8ff6cd38700
Pereverzev-Alexander <sanya.home01@gmail.com>
2016-04-23 20:29:48 +0300

    First parser json

---
+++ Test.py
@@ -0,0 +1,21 @@
+import json
+
+jsonNameSCOPUS = 'scopus_0.json'
+jsonNameWOS = 'wos_0.json'
+jsonNameSPIN = 'spin_19xx-1995_org_items_192.json'
+
+with open(jsonNameSCOPUS) as data_file:
+    data = json.load(data_file)
+    data_file.close()
+
+print("Count objects: %i" % len(data))
+
+i = 0
+for key in data:
+    if i < 9:
+        print("Object: %s" % key)
+        i += 1
+    else:
+        print("To much output!")
+        break
+print("Done!")
