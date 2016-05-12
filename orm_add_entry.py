5ba8a6f1d2f318cccca760283e466ba4c53e8265
amvasil <vasil94@inbox.ru>
2016-05-13 00:40:36 +0300

    Add ORM create sample table

--- orm_add_entry.py
+++ orm_add_entry.py
@@ -1,7 +1,31 @@
 import psycopg2
+import peewee
+from peewee import *
+
+db = PostgresqlDatabase(
+    'postgres',  # Required by Peewee.
+    user='postgres',  # Will be passed directly to psycopg2.
+    password='admin',
+    host='localhost',
+)
 
 try:
-    conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='admin'")
+    db.connect()
 except:
-    print ("I am unable to connect to the database")
-print("Done")
+    print("Failed to connect to database")
+    quit()
+print("Done connection")
+
+
+# ORM class for Author
+# just an example by now
+class AuthorModel(Model):
+    id = PrimaryKeyField()
+    name = CharField()
+
+    class Meta:
+        database = db
+
+
+AuthorModel.create_table()
+
