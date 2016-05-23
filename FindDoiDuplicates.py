45cf3bd62ac945e006df816dc540ab9918cfb39d
amvasil <vasil94@inbox.ru>
2016-05-23 12:31:01 +0300

    Сравнения по ДОИ работает быстро
    Сравнение по дои и году работает очень медленно

--- FindDoiDuplicates.py
+++ FindDoiDuplicates.py
@@ -1,31 +1,115 @@
 from FilterSelectOrm import *
 
+
+# class to store found duplicates
+class Duplicate:
+    def __str__(self):
+        res = "duplicate: "
+        if self.scopus_p is not None:
+            res += " scopus: " + str(self.scopus_p.id)
+        if self.wos_p is not None:
+            res += " wos: " + str(self.wos_p.id)
+        if self.spin_p is not None:
+            res += " spin: " + str(self.spin_p.id)
+        return res
+
+    def __repr__(self):
+        res = "repr: "
+        return res
+
+    scopus_p = None
+    wos_p = None
+    spin_p = None
+
+    def count_instances(self):
+        count = 0
+        if self.scopus_p is not None:
+            count += 1
+        if self.wos_p is not None:
+            count += 1
+        if self.spin_p is not None:
+            count += 1
+        return count
+
+
+# insert internal function
+def insert_internal(p, duplicate):
+    if p.source == "scopus":
+        duplicate.scopus_p = p
+        return
+    if p.source == "wos":
+        duplicate.wos_p = p
+        return
+    if p.source == "spin":
+        duplicate.spin_p = p
+
+
+# insert article into respective Duplicate class
+def insert_duplicate(p, dupl_p, dupl_list):
+    if p.source == dupl_p.source:
+        print("Trying to insert duplicate from same source")
+        return
+    for d in dupl_list:
+        if dupl_p.source == "scopus" and d.scopus_p is not None and d.scopus_p.id == dupl_p.id:
+            insert_internal(p, d)
+            return
+        if dupl_p.source == "wos" and d.wos_p is not None and d.wos_p.id == dupl_p.id:
+            insert_internal(p, d)
+            return
+        if dupl_p.source == "spin" and d.spin_p is not None and d.spin_p.id == dupl_p.id:
+            insert_internal(p, d)
+            return
+    # another duplicate not found, insert new one
+    d = Duplicate()
+    insert_internal(p, d)
+    insert_internal(dupl_p, d)
+    dupl_list.append(d)
+
 # find all articles with same doi
 doi_dict = {}
-same_doi_publ = []
+duplicates = []
+
+i = 0
 # insert all articles from scopus, use doi as key
 for p in db_select_all("scopus"):
+    i += 1
+    print(i, len(duplicates))
     if p.doi == "":
         continue
-    doi_dict[p.doi] = ("scopus",p.id)
+    doi_dict[p.doi] = p
+
 # try to insert articles from wos
 for p in db_select_all("wos"):
+    i += 1
+    print(i, len(duplicates))
     if p.doi == "":
         continue
     if p.doi in doi_dict:
-        same_doi_publ.append(p)
+        insert_duplicate(p, doi_dict[p.doi], duplicates)
     else:
-        doi_dict[p.doi] = ("wos", p.id)
+        doi_dict[p.doi] = p
+
 # try to insert articles from spin
 for p in db_select_all("spin"):
+    i += 1
+    print(i, len(duplicates))
     if p.doi == "":
         continue
     if p.doi in doi_dict:
-        same_doi_publ.append(p)
+        insert_duplicate(p, doi_dict[p.doi], duplicates)
     else:
-        doi_dict[p.doi] = ("spin", p.id)
+        doi_dict[p.doi] = p
 
 
-for o in same_doi_publ:
-    print(o.title, o.source, o.doi)
-print("\nFound total  DOI duplicates: "+str(len(same_doi_publ)))
+# test
+print("\nFound total  DOI duplicates: "+str(len(duplicates)))
+count_double = 0
+count_triple = 0
+for d in duplicates:
+    c = d.count_instances()
+    if c == 2:
+        count_double += 1
+    if c == 3:
+        count_triple += 1
+
+print("In 2 databases: "+str(count_double)+", In 3 databases: "+str(count_triple)+", Total: "+str(count_double+count_triple))
