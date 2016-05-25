6a9f65b17340f1a1d0ed9eeb5a9f00109f36ad78
Pereverzev-Alexander <sanya.home01@gmail.com>
2016-05-25 05:38:52 +0300

    Добавлена функция сравнения по количеству авторов

--- ComparisonFunction.py
+++ ComparisonFunction.py
@@ -1,5 +1,15 @@
 from FilterSelectOrm import db_select_year_range
 
+# find articles with the same count author
+def find_same_num_authors(year_min, year_max, source1, source2):
+    lst = []
+    lst_1 = db_select_year_range(year_min, year_max, source1)
+    lst_2 = db_select_year_range(year_min, year_max, source2)
+    for p1 in lst_1:
+        for p2 in lst_2:
+            if p1.num_authors == p2.num_authors:
+                lst.append((p1,p2))
+    return lst
 
 # returns all articles published within given range
 # from given sources with same pages range
@@ -13,9 +23,9 @@ def find_same_pages(year_min, year_max, source1, source2):
                 lst.append((p1,p2))
     return lst
 
-
 # test function
+list_pub = find_same_num_authors(2001, 2001, "spin", "wos")
+print("Found with same count authors: "+str(len(list_pub)))
+
 list_pub = find_same_pages(2000, 2000, "scopus", "wos")
 print("Found with same pages range: "+str(len(list_pub)))
-
-
