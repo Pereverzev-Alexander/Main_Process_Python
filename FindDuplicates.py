c9feeca7156a8422becc049043a36cac0a92ce53
amvasil <vasil94@inbox.ru>
2016-06-05 12:16:21 +0300

    add commit, update readme

--- FindDuplicates.py
+++ FindDuplicates.py
@@ -2,7 +2,6 @@ from DbInteractions import *
 import re
 
 
-# TO BE REFACTORED
 def equals_doi(p1,p2):
     """ Сравнивает две статьи по ДОИ
         :parameter p1: первая статья(объект класса Publication)
@@ -10,9 +9,7 @@ def equals_doi(p1,p2):
         :return True: статьи совпали по ДОИ
         :return False: статьи  не совпали по ДОИ либо ДОИ отсутсвует
     """
-    if p1.doi == "None" or p1.doi == "" or p1.doi == "0":
-        return False
-    if p2.doi == "None" or p2.doi == "" or p2.doi == "0":
+    if p1.doi == "" or p2.doi == "":
         return False
     return p1.doi == p2.doi
 
@@ -62,6 +59,7 @@ def find_duplicates_internal(pubs1, pubs2):
     for p in pubs1:
         for comp in pubs2:
             if internal_compare_publications(p, comp):
+                # insert found duplicate into DB
                 duplicates_count += db_insert_duplicate(p, comp)
     return duplicates_count
 
@@ -69,21 +67,29 @@ def find_duplicates_internal(pubs1, pubs2):
 def find_grouping():
     """ Функция определения совпадающих статей.
     """
+    # first year of papers to be compared
     year_inf = 1950
+    # last year of papers to be compared
     year_sup = 2016
     duplicates_count = 0
-    lst = []
     for year in range(year_inf, year_sup+1):
-        ptable = {"scopus":[], "wos":[], "spin":[]}
+        # temporal storage for papers lists
+        ptable = {"scopus": [], "wos": [], "spin": []}
+        # compare papers in given year and year before it
         for y in [year - 1, year]:
             for source in ptable:
+                # fill 2*3 table with lists of papers
                 ptable[source].append(db_select_year(y, source))
         for i1 in [0, 1]:
             for i2 in [0, 1]:
+                # do not compare papers in previous year because they
+                # were compared on previous iteration
                 if i1 == 0 and i2 == 0:
                     continue
+                # compare all pairs
                 duplicates_count += find_duplicates_internal(ptable["scopus"][i1], ptable["wos"][i2])
                 duplicates_count += find_duplicates_internal(ptable["scopus"][i1], ptable["spin"][i2])
                 duplicates_count += find_duplicates_internal(ptable["spin"][i1], ptable["wos"][i2])
+                # total (4-1)*3 = 9 comparisons
 
         print("Checking year", year, " Total duplicates:", str(duplicates_count))
